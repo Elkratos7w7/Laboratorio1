7cc24c5cf2519719e5e4949a3876ea912764c69d
jerry <jerrybalam2@gmail.com>
2023-03-29 09:42:26 -0600

    Se agrego una multiplicaion

--- laboratorio.py
+++ laboratorio.py
@@ -5,3 +5,17 @@ no2 = int  (input ("Ingrese un segundo digeto para realizar la suma"))
 print (no2)
 resultado = no1 + no2
 print (input (f"El resultado de la suma es {resultado}"))
+
+no3 = int (input ("Ingrese un valor para empezar una Resta "))
+print (no3)
+no4 = int  (input ("Ingrese un segundo digeto para realizar la resta"))
+print (no4)
+resultadoderesta = no3 - no4
+print (input (f"El resultado de la resta es {resultadoderesta}"))
+
+no5 = int (input ("Ingrese un valor para empezar una multiplicación "))
+print (no5)
+no6 = int  (input ("Ingrese un segundo digeto para realizar la multiplicación"))
+print (no6)
+resultadodelamulti = no5 * no6
+print (input (f"El resultado de la resta es {resultadodelamulti}"))
