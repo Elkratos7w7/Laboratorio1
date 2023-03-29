6e7741fcc57a309c37eb93d307e7545b61396fd5
jerry <jerrybalam2@gmail.com>
2023-03-29 09:51:28 -0600

    se hicieron cambios

--- laboratorio.py
+++ laboratorio.py
@@ -8,7 +8,7 @@ print (input (f"El resultado de la suma es {resultado}"))
 
 no3 = int (input ("Ingrese un valor para empezar una Resta "))
 print (no3)
-no4 = int  (input ("Ingrese un segundo digeto para realizar la resta"))
+no4 = int  (input ("Ingrese un segundo digito para realizar la resta"))
 print (no4)
 resultadoderesta = no3 - no4
 print (input (f"El resultado de la resta es {resultadoderesta}"))
@@ -18,7 +18,7 @@ print (no5)
 no6 = int  (input ("Ingrese un segundo digeto para realizar la multiplicación"))
 print (no6)
 resultadodelamulti = no5 * no6
-print (input (f"El resultado de la resta es {resultadodelamulti}"))
+print (input (f"El resultado de la multiplicacion es {resultadodelamulti}"))
 
 no7 = int (input ("Ingrese un valor para empezar una Division "))
 print (no7)
