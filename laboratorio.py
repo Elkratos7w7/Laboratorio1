84563e9b9e466e44a946ecc2bb3409b45f19641f
jerry <jerrybalam2@gmail.com>
2023-03-29 09:45:12 -0600

    se agrego la segunda instruccion de la divison

--- laboratorio.py
+++ laboratorio.py
@@ -22,3 +22,5 @@ print (input (f"El resultado de la resta es {resultadodelamulti}"))
 
 no7 = int (input ("Ingrese un valor para empezar una Division "))
 print (no7)
+no8 = int  (input ("Ingrese un segundo digeto para realizar la division"))
+print (no8)
